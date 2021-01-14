b1e4f8886f2e813c48bdc114a03fe159aa01cea8
igor0212 <igor_0212@hotmail.com>
2021-01-13 22:32:18 -0300

    Compilando projeto

--- tp1/main.c
+++ tp1/main.c
@@ -4,36 +4,30 @@
 
 typedef struct {
     int idx, length;
-    char name[50];
-}
-
-thread_arg, *ptr_thread_arg;
+}thread_arg, *ptr_thread_arg;
 
 pthread_t threads[2];
 
 void *thread_func(void *arg) {
     ptr_thread_arg targ = (ptr_thread_arg)arg;
-
     int i;
-
-    for(i=targ->idx; i<(targ->idx + targ->length); i++) {
-        printf('Thread %d – value %d\n', pthread_self(), i);
-    }
+    for(i=targ->idx; i<(targ->idx + targ->length); i++)
+        printf("Thread %ld – value %d\n", pthread_self(), i);
 }
 
-int main(int argc, char **argv) {
+void main(void)
+{
     thread_arg arguments[2];
-    
     int i;
-    
+
     for(i=0; i<2; i++) {
         arguments[i].idx = i * 10;
         arguments[i].length = 10;
         pthread_create(&(threads[i]), NULL, thread_func,
         &(arguments[i]));
     }
-
+ 
     for(i=0; i<2; i++) {
         pthread_join(threads[i], NULL);
     }
-}
+};
