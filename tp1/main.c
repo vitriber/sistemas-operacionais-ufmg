c5a3d10c825734d7f14cda9dc3a2e0c493e88ae6
igor0212 <igor_0212@hotmail.com>
2021-01-18 00:01:00 -0300

    Usando mutex

--- tp1/main.c
+++ tp1/main.c
@@ -1,41 +1,75 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
+#include <unistd.h>
+#include<string.h>
 
 typedef struct {
     const char *name;
     pthread_t thread;
 } friend;
 
-pthread_mutex_t forno;
+pthread_mutex_t oven;
+pthread_cond_t queue;
+pthread_cond_t rules;
+int it;
 
-pthread_cond_t enfileirar_membro;
-pthread_cond_t regras_precedencia;
+void *friends_func(void *arg) {      
 
-void *friends_func(void *arg) {
-
-    friend *p_ptr = NULL;
-    p_ptr = (friend*)arg;
-
+    friend *p_ptr = (friend*)arg; 
+    int j = 0;       
     
-    printf("Thread %s – value %ld\n", p_ptr->name, pthread_self());
+    //printf("Thread %s – value %ld\n", p_ptr->name, pthread_self());
+
+    while(j < it) {       
+         
+        printf("%s quer usar o forno\n", p_ptr->name);
+        sleep(3);
+ 
+        printf("%s começa a esquentar algo\n", p_ptr->name);
+        sleep(1);
+ 
+        pthread_mutex_lock(&oven);         
+        printf("%s vai comer\n", p_ptr->name);
+        sleep(6);
+        pthread_mutex_unlock(&oven);      
+                
+        printf("%s voltou para o trabalho\n", p_ptr->name);
+        sleep(4);         
+
+        j++;
+    }
     
 }
 
-void main(void)
-{
-    const char *nameList[] = { "Sheldon", "Leonard", "Howard", "Stuart", "Kripke", "Any", "Bernadette", "Penny" };
-    friend** friends = malloc(sizeof(*friends)*5);
+void main( int argc, char *argv[] )  {        
 
-    int i;
+    if( argc != 2 ) {
+      printf("Error: Informar, como parâmetro, quantidade de iteração.\n");
+      return;
+   }
 
-    for (i = 0; i < 9; i++) {
-        friends[i] = malloc(sizeof(*friends[i]));
-        friends[i]->name = nameList[i];
-        pthread_create( &friends[i]->thread, NULL, friends_func, friends[i]);
+    it = atoi(argv[1]);   
+
+    //const char *nameList[] = { "Sheldon", "Leonard", "Howard", "Stuart", "Kripke", "Any", "Bernadette", "Penny" };
+    const char *nameList[] = { "Sheldon", "Leonard"};
+    int sizeList = sizeof nameList / sizeof nameList[0];
+    int i = 0;
+    
+    friend** friends = malloc(sizeof(*friends)*sizeList);
+    
+
+    if (pthread_mutex_init(&oven, NULL) != 0)
+    {
+        printf("\n mutex init failed\n");
+        return;
     }
- 
-    // for(i=0; i<2; i++) {
-    //     pthread_join(threads[i], NULL);
-    // }
+
+    for (i = 0; i < sizeList; i++) {        
+        friends[i] = malloc(sizeof(*friends[i]));        
+        friends[i]->name = nameList[i];        
+        pthread_create(&friends[i]->thread, NULL, friends_func, friends[i]);        
+    }
+    
+    pthread_exit(NULL);
 };
